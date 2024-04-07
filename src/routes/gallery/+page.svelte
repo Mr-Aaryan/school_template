af3a6474bafa800b03829a639c18440c67655f11
Mr-Aaryan <kc.aryan3536@gmail.com>
2024-04-07 22:14:27 +0545

    gallery

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,101 @@
+<script>
+    import { Gallery } from "flowbite-svelte";
+    import { Carousel, Indicator } from 'flowbite-svelte';
+    let image;
+
+    const images = [
+    {
+      alt: 'School Building',
+      src: 'http://hdpsbahu.com/images/Pages/school%20building.jpg',
+      title: 'School Building'
+    },
+    {
+      alt: 'School Building',
+      src: 'https://c1.wallpaperflare.com/preview/968/730/441/building-cheyenne-photos-high-school.jpg',
+      title: 'school building'
+    },
+    {
+      alt: 'School Building',
+      src: 'https://www.jkeducate.co.uk/app/uploads/2022/08/classroom-2093744_1920.jpg.webp',
+      title: 'School Building'
+    },
+    {
+      alt: 'School Building',
+      src: 'https://c1.wallpaperflare.com/preview/968/730/441/building-cheyenne-photos-high-school.jpg',
+      title: 'school building'
+    },
+    {
+      alt: 'School Building',
+      src: 'http://hdpsbahu.com/images/Pages/school%20building.jpg',
+      title: 'School Building'
+    },
+    {
+      alt: 'School Building',
+      src: 'https://www.jkeducate.co.uk/app/uploads/2022/08/classroom-2093744_1920.jpg.webp',
+      title: 'School Building'
+    },
+  ];
+  const images1 = [
+    {
+      alt: 'Gallery',
+      src: 'https://excelpublicschool.com/EPS/Content/img/academics/academicsSchedule/2.jpg',
+      title: 'gallery'
+    },
+    {
+      alt: 'Gallery',
+      src: 'https://bournetrust.s3.amazonaws.com/uploads/asset_image/2_291_e.jpg?t=1681806499',
+      title: 'gallery'
+    },
+    {
+      alt: 'Gallery',
+      src: 'https://broxbourne.s3.amazonaws.com/uploads/asset_image/2_13_e.jpg?t=1627639461',
+      title: 'gallery'
+    },
+    {
+      alt: 'Gallery',
+      src: 'https://newageworldschool.com/assets/images/inside-gallery-page/NewAge-world-School-Gallery-Image-1.jpg',
+      title: 'gallery'
+    },
+    {
+      alt: 'Gallery',
+      src: 'https://excelpublicschool.com/EPS/FileUploads/6ac0faef-fba2-4346-9533-32c8908f9fc1@IMG8297.JPG',
+      title: 'gallery'
+    },
+    {
+      alt: 'Gallery',
+      src: 'https://www.jkeducate.co.uk/app/uploads/2022/08/classroom-2093744_1920.jpg.webp',
+      title: 'gallery'
+    },
+    {
+      alt: 'Gallery',
+      src: 'https://content.jdmagicbox.com/comp/mysore/c8/0821px821.x821.090522121743.j1c8/catalogue/excel-public-school-belavadi-mysore-cbse-schools-arfiv.jpg',
+      title: 'gallery'
+    },
+    {
+      alt: 'Gallery',
+      src: 'https://www.excelpublicschool.com/EPS/Content/img/learningBeyond/InnovationLab/6.jpg',
+      title: 'gallery'
+    },
+    {
+      alt: 'Gallery',
+      src: 'https://5.imimg.com/data5/DJ/PV/GLADMIN-64970456/computer-lab-class.png',
+      title: 'gallery'
+    },
+    
+  ];
+</script>
+
+<main class="mx-auto py-6">
+    <div >
+        <Carousel {images} let:Indicators let:Controls on:change={({ detail }) => (image = detail)}>
+            <Controls />
+            <Indicators />
+          </Carousel>
+    </div>
+    <div class="container mx-auto px-2">
+        <h2 class="py-8 text-4xl"><span class="pb-1 border-b border-black">Our Gall</span>ery</h2>
+        <div> 
+              <Gallery class="gap-4 grid-cols-2 md:grid-cols-3" items={images1} />
+        </div>    
+    </div>
+</main>
